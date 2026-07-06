d34fcb0ec27826e11d58152bb714d04ab3628473
Ankush Rane <ankushrane2024@gmail.com>
2026-07-06 15:19:33 +0530

    Fix ServiceRunner indentation

--- hedge/deployment/service_runner.py
+++ hedge/deployment/service_runner.py
@@ -24,6 +24,16 @@ from hedge.models.core_interfaces import SystemClock
 
 logger = logging.getLogger("system")
 
+class PaperMarketDataProvider:
+    def __init__(self, clock):
+        self.clock = clock
+    def get_latest_data(self):
+        return {
+            "spot_price": 50000.0, "funding": 0.01, "timestamp": self.clock.now(),
+            "open_interest": 1000.0, "volume": 5000.0, "iv": 0.6,
+            "call_greeks": {"delta": 0.5, "gamma": 0.05, "vega": 10.0}
+        }
+
 class ServiceRunner:
     """
     Master entrypoint for ARES.
@@ -41,16 +51,6 @@ class ServiceRunner:
     def _signal_handler(self, sig, frame):
         logger.info(f"Received termination signal ({sig}). Initiating graceful shutdown...")
         self.running = False
-
-class PaperMarketDataProvider:
-    def __init__(self, clock):
-        self.clock = clock
-    def get_latest_data(self):
-        return {
-            "spot_price": 50000.0, "funding": 0.01, "timestamp": self.clock.now(),
-            "open_interest": 1000.0, "volume": 5000.0, "iv": 0.6,
-            "call_greeks": {"delta": 0.5, "gamma": 0.05, "vega": 10.0}
-        }
 
     def setup_orchestrator_and_validator(self):
         market_data = PaperMarketDataProvider(self.clock)
